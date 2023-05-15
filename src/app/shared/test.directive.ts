26afa4914ed81592f7961408e3bc194cae48f70d
Rafal Brzoska <rafal@brzoska.net.pl>
2023-05-15 12:40:10 +0200

    test directive example usage

--- src/app/shared/test.directive.ts
+++ src/app/shared/test.directive.ts
@@ -1,13 +1,19 @@
-import { Directive, ElementRef, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appTest]'
 })
 export class TestDirective {
 
-  @Input() appTest!: boolean;
+  @Input()
+  set appTest(val: boolean) {
+    this._vcr.clear()
+    if(val) {
+      this._vcr.createEmbeddedView(this._templ)
+    }
+  };
   constructor( private _templ: TemplateRef<any>, private _vcr: ViewContainerRef) {
-    _vcr.createEmbeddedView(_templ)
   }
+
 
 }
